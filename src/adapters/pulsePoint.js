137ae294f2fcd61e4c4abcbb87ebcfd6b289c694
Harshad Mane <harshad.mane@pubmatic.com>
2016-12-02 18:35:47 +0530

    Change for utilForEachGeneratedKey

--- src/adapters/pulsePoint.js
+++ src/adapters/pulsePoint.js
@@ -79,20 +79,12 @@ adapterManagerRegisterAdapter((function() {
 
 			utilLoadScript(jsLibURL, function(){						
 				utilForEachGeneratedKey(
+					adapterID,
+					slotConfigMandatoryParams,
 					activeSlots, 
 					keyGenerationPattern, 
 					keyLookupMap, 
 					function(generatedKey, kgpConsistsWidthAndHeight, currentSlot, keyConfig, currentWidth, currentHeight){
-
-						if(!keyConfig){
-							utilLog(adapterID+': '+generatedKey+constCommonMessage08);
-							return;
-						}
-
-						if(!utilCheckMandatoryParams(keyConfig, slotConfigMandatoryParams, adapterID)){
-							utilLog(adapterID+': '+generatedKey+constCommonMessage09);
-							return;
-						}
 
 						var adSlotSizes = kgpConsistsWidthAndHeight ? [[currentWidth, currentHeight]] : currentSlot[constAdSlotSizes];
 						for(var n=0, l=adSlotSizes.length; n<l; n++){
