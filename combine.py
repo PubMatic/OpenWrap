349382d24fad7719d0709c9c35a3494712e05651
Harshad Mane <harshad.mane@pubmatic.com>
2016-12-06 16:37:26 +0530

    added sovrn in combine.py

--- combine.py
+++ combine.py
@@ -4,7 +4,7 @@ import os
 #For example, if rubicon is not the SSP you work with, you can remove 'adapters/rubicon.js' from below array. 
 #The same way if you want to add any partner adapter to below array, just append it to this array. Adapters for supported partners are present in src/adapters directory.
 #Please refer to documentation if adapter you are looking to add is not present in src/adapters directory.
-adapterFiles = ['adapters/pubmatic.js', 'adapters/appnexus.js', 'adapters/bRealTime.js', 'adapters/districtM.js', 'adapters/openx.js', 'adapters/rubicon.js', 'adapters/rubiconFastlane.js', 'adapters/indexExchange.js', 'adapters/yieldbot.js', 'adapters/adForm.js', 'adapters/pulsePoint.js']
+adapterFiles = ['adapters/pubmatic.js', 'adapters/appnexus.js', 'adapters/bRealTime.js', 'adapters/districtM.js', 'adapters/openx.js', 'adapters/rubicon.js', 'adapters/rubiconFastlane.js', 'adapters/indexExchange.js', 'adapters/yieldbot.js', 'adapters/adForm.js', 'adapters/pulsePoint.js', 'adapters/sovrn.js']
 
 #These are mandatory files for open wrapper code generation. Please do not remove any file from this array.
 fileNames = ['commonVariables.js', 'util.js', 'adapterManager.js', 'bidManager.js']
