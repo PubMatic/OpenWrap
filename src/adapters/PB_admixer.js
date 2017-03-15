b6cc1c1c8040f733adfcec8f228cac6c7fb5db08
Harshad Mane <harshad.mane@pubmatic.com>
2017-03-15 11:13:12 +0530

    added PB_admixer and PB_sekindoMU adapters

---
+++ src/adapters/PB_admixer.js
@@ -0,0 +1,10 @@
+adapterManagerRegisterAdapter((function(){
+	var adapterID = 'PB_admixer';
+	return {		
+		fB: function(){},	
+		dC: utilDisplayCreative,
+		ID: function(){
+			return adapterID;
+		}
+	};
+})());
---
+++ src/adapters/PB_sekindoUM.js
@@ -0,0 +1,10 @@
+adapterManagerRegisterAdapter((function(){
+	var adapterID = 'PB_sekindoUM';
+	return {		
+		fB: function(){},	
+		dC: utilDisplayCreative,
+		ID: function(){
+			return adapterID;
+		}
+	};
+})());
