59ca6fe4a82f59ad5355912401509a8629772830
Harshad Mane <harshad.mane@pubmatic.com>
2017-06-07 13:04:52 +0530

    added new methods in slot

--- src_new/slot.js
+++ src_new/slot.js
@@ -1,18 +1,128 @@
 var CONSTANTS = require("./constants.js");
+var UTIL = require("./util.js");
 
 function Slot(name){
 	this.name = name;
-	this.displayFunctonCalled = false;
-	this.refreshFunctionCalled = false;
-	this.argumentsPassed = [];
-	this.PubAdServerObject = null;
 	this.status = CONSTANTS.SLOT_STATUS.CREATED;
 	this.divID = "";
-	this.AdUnitID = "";
-	this.AdUnitIndex = 0;
+	this.adUnitID = "";
+	this.adUnitIndex = 0;
 	this.sizes = [];
-	this.keyValues = null;
+	this.keyValues = {};
+	this.arguments = [];
+	this.pubAdServerObject = null;
+	this.displayFunctionCalled = false;
+	this.refreshFunctionCalled = false;	
 }
 
+Slot.prototype.getName = function(){
+	return this.name;
+};
 
+Slot.prototype.setStatus = function(status){
+	//check is it a valid status
+	this.status = status;
+	return this;
+};
 
+Slot.prototype.getStatus = function(){
+	return this.status;
+};
+
+Slot.prototype.setDivID = function(divID){
+	//check is it a valid divID, string
+	this.divID = divID;
+	return this;
+};
+
+Slot.prototype.getDivID = function(){
+	return this.divID;
+};
+
+Slot.prototype.setAdUnitID = function(value){
+	//check is it a valid divID, string
+	this.adUnitID = value;
+	return this;
+};
+
+Slot.prototype.getAdUnitID = function(){
+	return this.adUnitID;
+};
+
+Slot.prototype.setAdUnitIndex = function(value){
+	//check is it a valid divID, string or number 
+	this.adUnitIndex = value;
+	return this;
+};
+
+Slot.prototype.getAdUnitIndex = function(){
+	return this.adUnitIndex;
+};
+
+Slot.prototype.setSizes = function(value){
+	//check is it a valid value, array
+	this.sizes = value;
+	return this;
+};
+
+Slot.prototype.getSizes = function(){
+	return this.sizes;
+};
+
+Slot.prototype.setKeyValue = function(key, value){
+	//check is it a valid value, array
+	this.keyValues[key] = value;
+	return this;
+};
+
+Slot.prototype.setKeyValues = function(value){
+	//check is it a valid value, array
+	this.keyValues = value;
+	return this;
+};
+
+Slot.prototype.getkeyValues = function(){
+	return this.keyValues;
+};
+
+Slot.prototype.setArguments = function(value){
+	//check is it a valid value, array
+	this.arguments = value;
+	return this;
+};
+
+Slot.prototype.getArguments = function(){
+	return this.arguments;
+};
+
+Slot.prototype.setPubAdServerObject = function(value){
+	//check is it a valid value, array
+	this.pubAdServerObject = value;
+	return this;
+};
+
+Slot.prototype.getPubAdServerObject = function(){
+	return this.pubAdServerObject;
+};
+
+Slot.prototype.setDisplayFunctionCalled = function(value){
+	this.displayFunctionCalled = value;
+	return this;
+}
+
+Slot.prototype.isDisplayFunctionCalled = function(){
+	return this.displayFunctionCalled;
+}
+
+Slot.prototype.setRefreshFunctionCalled = function(value){
+	this.refreshFunctionCalled = value;
+	return this;
+}
+
+Slot.prototype.isRefreshFunctionCalled = function(){
+	return this.refreshFunctionCalled;
+}
+
+exports.createSlot = function(adapterID, kgpv){
+	return new Slot(name);
+};
