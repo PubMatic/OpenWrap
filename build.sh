a3f8bc499dda24440b928a68b9180558e9d107b5
Devendra <devendra.dhanal@pubmatic.com>
2017-06-13 12:17:44 +0530

    Changes in build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,15 @@ var argv = require('yargs').argv;
 
 #
 shell.exec("gulp clean");
-shell.cd("../Prebid.js/");
+
+var prebidRepoPath = argv.prebidpath || "../Prebid.js/";
+
+console.log("ARGV ==>", argv);
+
+if(shell.cd(prebidRepoPath).code !== 0) {
+	shell.echo("Couldnt change the dir to Prebid repo");
+	shell.exit(1);
+}
 
 var buildTaskName = "bundle";
 var webpackTaskName = "pack";
@@ -33,10 +41,27 @@ if ( argv.mode && argv.mode == "test-build") {
 # 	webpackTaskName = "web" + webpackTaskName;
 # }
 
-shell.exec("gulp " + webpackTaskName);
+if(shell.exec("gulp " + webpackTaskName + " --mode=" + argv.mode).code !== 0) {
+	shell.echo('Error: buidlinng of project failed');
+  	shell.exit(1);
+}
 
-shell.cd("../OpenWrap/")
+shell.cd("../OpenWrap/");
+if (argv.mode == "test-build") {
+	if(shell.exec("gulp test" + " --mode=" + argv.mode).code !== 0) {
+		shell.echo('Error: test cases failed');
+  		shell.exit(1);
+	}
+}
 
-shell.exec("gulp " + webpackTaskName)
+if(shell.exec("gulp " + webpackTaskName + " --mode=" + argv.mode).code !== 0) {
+	shell.echo('Error: webpack task failed');
+	shell.exit(1);
+}
 
-shell.exec("gulp " + buildTaskName);
+
+if(shell.exec("gulp " + buildTaskName + " --mode=" + argv.mode).code !== 0) {
+	shell.echo('Error: build task failed');
+	shell.exit(1);
+}
+
