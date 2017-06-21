8175e51e2d051ace2e990a18141cdaaed453f1e1
Harshad Mane <harshad.mane@pubmatic.com>
2017-06-21 13:04:13 +0530

    execute logger-pixel per impressionID

--- src_new/bidManager.js
+++ src_new/bidManager.js
@@ -2,13 +2,7 @@ var CONFIG = require("./config.js");
 var CONSTANTS = require("./constants.js");
 var util = require("./util.js");
 var bmEntry = require("./bmEntry.js")
-//var PWT = require("./owt").PWT;
-//var PWT = window.PWT;
 //PWT.bidIdMap = {}; // bidID => {slotID, adapterID}
-
-//todo: is it required ?
-var stringBidders = "bidsFromBidders";
-var stringBidSizes = "adSlotSizes";
 
 function createBidEntry(divID){
 	if(! util.isOwnProperty(PWT.bidMap, divID) ){
@@ -86,7 +80,7 @@ function storeBidInBidMap(slotID, adapterID, theBid, currentTime){
 		a: adapterID
 	};
 
-	if(bidDetails.getDefaultBidStatus() === 0){
+	if(theBid.getDefaultBidStatus() === 0){
 		/*utilVLogInfo(slotID, {
 			type: "bid",
 			bidder: adapterID + (CONFIG.getBidPassThroughStatus(adapterID) !== 0 ? '(PT)' : ''),
@@ -214,107 +208,78 @@ exports.displayCreative = function(theDocument, bidID){
 	}
 };
 
-// todo: accept PWT.bidMap as parameter
-// todo: changes required for new stucture
 exports.executeAnalyticsPixel = function(){
-
-	var selectedInfo = {},
-		outputObj = {},
-		firePixel = false,
-		impressionID = "",
-		pixelURL = CONFIG.getAnalyticsPixelURL()
+	var outputObj = {
+			s: []
+		},
+		pixelURL = CONFIG.getAnalyticsPixelURL(),
+		impressionIDMap = {} // impID => slots[]
 		;
 
 	if(!pixelURL){
 		return;
 	}
 
-	outputObj["s"] = [];
+	pixelURL = util.protocol + pixelURL + 'pubid=' + CONFIG.getPublisherId() +'&json=';
 
-	for(var key in PWT.bidMap){
+	outputObj[CONSTANTS.CONFIG.PUBLISHER_ID] = CONFIG.getPublisherId();
+	outputObj[CONSTANTS.LOGGER_PIXEL_PARAMS.TIMEOUT] = CONFIG.getTimeout();
+	outputObj[CONSTANTS.LOGGER_PIXEL_PARAMS.PAGE_URL] = decodeURIComponent(util.pageURL);//todo change it 
+	outputObj[CONSTANTS.LOGGER_PIXEL_PARAMS.TIMESTAMP] = util.getCurrentTimestamp();
+	outputObj[CONSTANTS.CONFIG.PROFILE_ID] = CONFIG.getProfileID();
+	outputObj[CONSTANTS.CONFIG.PROFILE_VERSION_ID] = CONFIG.getProfileDisplayVersionID();
 
-		if(! util.isOwnProperty(PWT.bidMap, key)){
-			continue;
-		}
-
-		var startTime = PWT.bidMap[key].getCreationTime();
-		if(util.isOwnProperty(PWT.bidMap, key) && PWT.bidMap[key].exp !== false && PWT.bidMap[key]["ae"] === true ){
-
-			PWT.bidMap[key].exp = false;
-
+	util.forEachOnObject(PWT.bidMap, function(slotID, bmEntry){
+		var startTime = bmEntry.getCreationTime();
+		if(bmEntry.getAnalyticEnabledStatus() && !bmEntry.getExpiredStatus()){			
 			var slotObject = {
-				"sn": key,
-				"sz": PWT.bidMap[key][stringBidSizes],
+				"sn": slotID,
+				"sz": bmEntry.getSizes(),
 				"ps": []
 			};
 
-			selectedInfo[key] = {};
+			bmEntry.setExpired();
+			var impressionID = bmEntry.getImpressionID();
+			impressionIDMap[ impressionID ]	= impressionIDMap[ impressionID ] || [];
 
-			var bidsArray = PWT.bidMap[key][stringBidders];
-			impressionID = PWT.bidMap[key][CONSTANTS.COMMON.IMPRESSION_ID];
+			util.forEachOnObject(bmEntry.adapters, function(adapterID, adapterEntry){
 
-			for(var adapter in bidsArray){
-
-				//if bid-pass-thru is set then do not log the stringBidders
-				// 1: do NOT log, do NOT auction
-				// 2: do log, do NOT auction
-				if(CONFIG.getBidPassThroughStatus(adapter) === 1){
-					continue;
+				if(CONFIG.getBidPassThroughStatus(adapterID) == 1){
+					return;
 				}
 
-				if(bidsArray[adapter] && bidsArray[adapter].bid ){
+				util.forEachOnObject(adapterEntry.bids, function(bidID, theBid){
+					var endTime = theBid.getReceivedTime();
+					//todo: take all these key names from constants
+					slotObject["ps"].push({
+						"pn": adapterID,
+						"bidid": bidID,
+						"db": theBid.getDefaultBidStatus(),
+						"kgpv": theBid.getKGPV(),
+						"psz": theBid.getWidth() + "x" + theBid.getHeight(),
+						"eg": theBid.getGrossEcpm(),
+						"en": theBid.getNetEcpm(),
+						"di": theBid.getDealID(),
+						"dc": theBid.getDealChannel(),
+						"l1": endTime - startTime,
+						"l2": 0,
+						"t": theBid.getPostTimeoutStatus() === false ? 0 : 1,
+						"wb": theBid.getWinningBidStatus() === true ? 1 : 0
+					});
+				})
+			});
 
-					for(var bidID in bidsArray[adapter].bid){
-						if(! util.isOwnProperty(bidsArray[adapter].bid, bidID)){
-							continue;
-						}
-						var theBid = bidsArray[adapter].bid[bidID];
-						var endTime = theBid.getReceivedTime();
-						//todo: take all these key names from constants
-						slotObject["ps"].push({
-							"pn": adapter,
-							"bidid": bidID,
-							"db": theBid.getDefaultBidStatus(),
-							"kgpv": theBid.getKGPV(),
-							"psz": theBid.getWidth() + "x" + theBid.getHeight(),
-							"eg": theBid.getGrossEcpm(),
-							"en": theBid.getNetEcpm(),
-							"di": theBid.getDealID(),
-							"dc": theBid.getDealChannel(),
-							"l1": endTime - startTime,
-							"l2": 0,
-							"t": theBid.getPostTimeoutStatus() === false ? 0 : 1,
-							"wb": theBid.getWinningBidStatus() === true ? 1 : 0
-						});
-						firePixel = true;
-					}					
-				}
-			}
-			outputObj["s"].push(slotObject);
+			impressionIDMap[ impressionID ].push(slotObject);
 		}
-	}
+	});
 
-	if(firePixel){			
-		outputObj[CONSTANTS.CONFIG.PUBLISHER_ID] = CONFIG.getPublisherId();
-		outputObj[CONSTANTS.LOGGER_PIXEL_PARAMS.TIMEOUT] = CONFIG.getTimeout();
-		outputObj[CONSTANTS.LOGGER_PIXEL_PARAMS.PAGE_URL] = decodeURIComponent(util.pageURL);//todo change it 
-		outputObj[CONSTANTS.LOGGER_PIXEL_PARAMS.TIMESTAMP] = util.getCurrentTimestamp();
-		outputObj[CONSTANTS.COMMON.IMPRESSION_ID] = encodeURIComponent(impressionID);
-		outputObj[CONSTANTS.CONFIG.PROFILE_ID] = CONFIG.getProfileID();
-		outputObj[CONSTANTS.CONFIG.PROFILE_VERSION_ID] = CONFIG.getProfileDisplayVersionID();
-
-		pixelURL += "pubid=" + CONFIG.getPublisherId()+"&json=" + encodeURIComponent(JSON.stringify(outputObj));
-	}
-
-	if(firePixel){
-		(new Image()).src = util.protocol + pixelURL;
-		//utilAjaxCall(
-		//	utilMetaInfo.protocol + pixelURL + 'pubid=' + CONFIG.getPublisherId(),
-		//	function(){},
-		//	JSON.stringify(outputObj),
-		//	{} // todo later
-		//);
-	}
+	util.forEachOnObject(impressionIDMap, function(impressionID, slots){
+		if(slots.length > 0){
+			outputObj.s = slots;
+			outputObj[CONSTANTS.COMMON.IMPRESSION_ID] = encodeURIComponent(impressionID);
+			(new Image()).src = pixelURL + encodeURIComponent(JSON.stringify(outputObj));
+		}
+	});
 };
 
 exports.executeMonetizationPixel = function(slotID, theBid){
@@ -325,7 +290,7 @@ exports.executeMonetizationPixel = function(slotID, theBid){
 	}
 
 	pixelURL += "pubid=" + CONFIG.getPublisherId();
-	pixelURL += "&purl=" + utilMetaInfo.u;//todo
+	pixelURL += "&purl=" + util.pageURL;//todo
 	pixelURL += "&tst=" + util.getCurrentTimestamp();
 	pixelURL += "&iid=" + encodeURIComponent(PWT.bidMap[slotID].getImpressionID());
 	pixelURL += "&bidid=" + encodeURIComponent(theBid.getBidID());
@@ -343,4 +308,3 @@ exports.executeMonetizationPixel = function(slotID, theBid){
 //todo
 // check if all bid comparisons are made on netEcpm
 // check data types in logger pixel
-// logger per impression id change
