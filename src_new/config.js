446114b978ab2647f49538f5280de61a922f0c56
Harshad Mane <harshad.mane@pubmatic.com>
2017-06-22 17:53:27 +0530

    added code to copy adapter-level params to slot-level

--- src_new/config.js
+++ src_new/config.js
@@ -55,7 +55,24 @@ exports.forEachAdapter = function(callback){
 };
 
 exports.initConfig = function(){
+	var ignoreAdapterLevelParams = {};
+	util.forEachOnObject(CONSTANTS.CONFIG, function(key, value){
+		ignoreAdapterLevelParams[value] = "";
+	});
+
 	util.forEachOnObject(config.adapters, function(adapterID, adapterConfig){
-		adapterConfig["NEW_"+adapterID] = adapterID;
+		if(adapterID.indexOf(CONSTANTS.COMMON.PREBID_PREFIX) == 0){
+			var adapterLevelParams = {};
+			util.forEachOnObject(adapterConfig, function(key, value){
+				if(!util.isOwnProperty(ignoreAdapterLevelParams, key)){
+					adapterLevelParams[ key ] = value;	
+				}				
+			});
+			util.forEachOnObject(adapterConfig[CONSTANTS.CONFIG.KEY_LOOKUP_MAP], function(kgpv, slotLevelParams){
+				util.forEachOnObject(adapterLevelParams, function(key, value){
+					slotLevelParams[ key ] = value;
+				});
+			});
+		}		
 	});
 };
