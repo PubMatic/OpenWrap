22612797ab8d96b8016150a392d1d989aea18340
Harshad Mane <harshad.mane@pubmatic.com>
2017-06-29 10:07:22 +0530

    added pulsepoint in config

--- src_new/conf.js
+++ src_new/conf.js
@@ -47,5 +47,19 @@ exports.adapters = {
 				placementId: "8801685"
 			}
 		}
+	},
+	pulsepoint: {
+		cp: "521732",	
+		rev_share: "0.0",
+		throttle: "100",
+		kgp: "_DIV_",
+		klm: {
+			"Div_1": {
+				ct: "76835"
+			},
+			"Div-2": {
+				ct: "147007"
+			}
+		}
 	}
 };
