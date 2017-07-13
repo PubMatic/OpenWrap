808f4cb015dbdb7e8a983da73c5307a8279812cb
Devendra <devendra.dhanal@pubmatic.com>
2017-07-13 11:51:53 +0530

    init-build.sh minor changes

--- init-build.sh
+++ init-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Steps
+# run sym links for node_modules folder
 # run npm install in current directory i.e. OpenWrap repo
 # run npm install in Prebid.js repo ( accept path for the same ? )
-# run sym links for node_modules folder
 # run build.sh with arguments accepted while executing this script
 
 if [ $# -eq 0 ]
@@ -36,24 +36,21 @@ if [ -z $mode ]
         echo "Please provide appropriate mode argument "
 fi
 
-#echo "%s %s " "$prebid_path" "$mode"
 OpenWrapNodeModules="${GLOBAL_OPENWRAP_PKG_JSON_DIR}/node_modules/"
 
-# echo "$OpenWrapNodeModules"
 
 function prebidNpmInstall() {
-  # echo "$1"
+
   cd $1
+
   PrebidJSNodeModules="${GLOBAL_PREBID_PKG_JSON_DIR}/node_modules/"
-  # echo $PrebidJSNodeModules
+
   ln -s "$PrebidJSNodeModules" "./node_modules"
+
   npm install
+
   cd ../OpenWrap/
 }
-
-#cat file.txt
-
-#echo $LOGNAME
 
 ln -s "$OpenWrapNodeModules" "./node_modules"
 
@@ -61,8 +58,4 @@ npm install
 
 prebidNpmInstall $prebid_path
 
-#cat file.txt
-
-
 ./build.sh --prebidpath=$prebid_path --mode=$mode
-
