f65c62fe6437516eaab5080fa4604629f0e7ca20
pm-shashank-jain <shashank.jain@pubmatic.com>
2018-09-06 12:21:02 +0530

    Code Review Changes

--- src_new/util.js
+++ src_new/util.js
@@ -329,24 +329,23 @@ exports.resizeWindow = function(theDocument, width, height, divId){
 	if(height && width){
 		try{
 			var defaultViewFrame = theDocument.defaultView.frameElement;
+			var elementArray=[];
 			if(divId){
 				var adSlot = document.getElementById(divId);
 				var adSlot_Div = adSlot.querySelector("div");
-				var adSlot_Div_iframe = adSlot_Div.querySelector("iframe");
-				[adSlot_Div,adSlot_Div_iframe].forEach(function(ele){
-					if(ele){
-						ele.width ="" +  width;
-						ele.height ="" + height;
-						ele.style.width = "" + width + "px";
-						ele.style.height = "" + height + "px";
-					}
-				});
+				elementArray.push(adSlot_Div);
+				elementArray.push(adSlot_Div.querySelector("iframe"));
 				defaultViewFrame = adSlot.querySelector("iframe");
 			}
-			defaultViewFrame.width ="" +  width;
-			defaultViewFrame.height ="" + height;
-			defaultViewFrame.style.width = "" + width + "px";
-			defaultViewFrame.style.height = "" + height + "px";
+			elementArray.push(defaultViewFrame);
+			elementArray.forEach(function(ele){
+				if(ele){
+					ele.width ="" +  width;
+					ele.height ="" + height;
+					ele.style.width = "" + width + "px";
+					ele.style.height = "" + height + "px";
+				}
+			});
 		}catch(e){
 			refThis.log("Creative-Resize; Error in resizing creative");
 		} // eslint-disable-line no-empty
