8e8a5ba669a848991b171169aa856d3e4cd4b16c
pm-shashank-jain <shashank.jain@pubmatic.com>
2019-02-22 12:25:37 +0530

    check for isString and isArray

--- src_new/controllers/gpt.js
+++ src_new/controllers/gpt.js
@@ -127,7 +127,7 @@ function getAdSlotSizesArray(divID, currentGoogleSlot) { // TDD, i/o : done
         // Below code is written in case  of responsive sizes and first size is fluid and we are sending single impression
         // for multiple sizes. As first size is fluid single impression will be passed as DIV@fxl and it will be 
         // ignored in pubmaticBidAdapter in prebid, hence below code move fluid size to last size preventing the above condition
-        if(typeof(sizeMapping[0]) == "string" || (sizeMapping[0].length == 1 && typeof(sizeMapping[0][0]) == "string")){
+        if(util.isString(sizeMapping[0]) || (util.isArray(sizeMapping[0]) && sizeMapping[0].length == 1 && util.isString(sizeMapping[0][0]))){
             sizeMapping.push(sizeMapping.shift());
         }
         util.log(sizeMapping);
