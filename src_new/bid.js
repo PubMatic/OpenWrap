f024764c827a4157d5be4f608fed872c959dec66
pm-shashank-jain <shashank.jain@pubmatic.com>
2020-03-11 10:42:38 +0530

    Fix for updating bid id in case of video bid

--- src_new/bid.js
+++ src_new/bid.js
@@ -340,9 +340,11 @@ Bid.prototype.setcacheUUID = function(cacheUUID){
 // This function is used to update the bid in case of video bid
 // this should only be called if bid is video so that there is no discrepancy in tracker and logger for bid Id
 Bid.prototype.updateBidId = function(slotID){
-	var bidId = window.PWT.bidMap[slotID].adapters[this.adapterID].bids[Object.keys(window.PWT.bidMap[slotID].adapters[this.adapterID].bids)[0]].bidID;
-	if(bidId && this.adFormat == CONSTANTS.FORMAT_VALUES.VIDEO){
-		this.bidID = bidId;
+	if(window.PWT.bidMap[slotID] && window.PWT.bidMap[slotID].adapters && window.PWT.bidMap[slotID].adapters.length>0){
+		var bidId = window.PWT.bidMap[slotID].adapters[this.adapterID].bids[Object.keys(window.PWT.bidMap[slotID].adapters[this.adapterID].bids)[0]].bidID;
+		if(bidId && this.adFormat == CONSTANTS.FORMAT_VALUES.VIDEO){
+			this.bidID = bidId;
+		}
 	}
 	return this;
 };
