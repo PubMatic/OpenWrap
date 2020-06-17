a5774b7ddd6c5af16990282417ba1941d922bcc8
Harshad Mane <harshad.mane@pubmatic.com>
2020-06-16 17:18:40 -0700

    REMOVE THIS: added some code only for testing

--- src_new/adapters/prebid.js
+++ src_new/adapters/prebid.js
@@ -522,9 +522,10 @@ function pushAdapterParamsInAdunits(adapterID, generatedKey, impressionID, keyCo
 		case "pubmatic":
 		case "pubmatic2":
 			slotParams["publisherId"] = adapterConfig["publisherId"];
-			slotParams["adSlot"] = slotParams["slotName"] || generatedKey;
+			slotParams["adSlot"] = "pubmatic_test2@300x250"; //todo: remove this
+			// slotParams["adSlot"] = slotParams["slotName"] || generatedKey;
 			slotParams["wiid"] = impressionID;
-			slotParams["profId"] = adapterID == "pubmatic2"? adapterConfig["profileId"]: CONFIG.getProfileID();
+			// slotParams["profId"] = adapterID == "pubmatic2"? adapterConfig["profileId"]: CONFIG.getProfileID(); // todo: uncomment it
 			/* istanbul ignore else*/
 			if(adapterID != "pubmatic2" && window.PWT.udpv){
 				slotParams["verId"] = CONFIG.getProfileDisplayVersionID();
