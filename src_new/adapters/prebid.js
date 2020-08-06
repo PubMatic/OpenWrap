e064ec56ab83fdd00144e3ba819682021f5567a0
Harshad Mane <harshad.mane@pubmatic.com>
2020-08-05 17:30:59 -0700

    Revert "set the required keys in bid object"
    This reverts commit c5bb97b5a8b2edd3ae66ed2e951b2f87444c31e2.

--- src_new/adapters/prebid.js
+++ src_new/adapters/prebid.js
@@ -1070,16 +1070,9 @@ exports.fetchBids = fetchBids;
 
 // returns the highest bid and its key value pairs
 function getBid(divID){
-	var wb = window[pbNameSpace].getHighestCpmBids([divID])[0] || null;
-	if(wb){
-		wb.adHtml = wb.ad;
-		wb.adapterID = wb.bidder;
-		wb.netEcpm = wb.cpm;
-		wb.grossEcpm = wb.originalCpm;
-	}
-
+	//todo: do we have exactly same key-names as of required in findWinningBidAndGenerateTargeting in custom.js?
 	return {
-		wb: wb,
+		wb: window[pbNameSpace].getHighestCpmBids([divID]) || null, 
 		kvp: window[pbNameSpace].getAdserverTargetingForAdUnitCode([divID]) || null
 	};
 }
