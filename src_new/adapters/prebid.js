21855215ffe8c63e68c8493c73fd265e71bacd7a
Harshad Mane <harshad.mane@pubmatic.com>
2020-09-23 14:55:21 -0700

    set bid-precision 1.5 ==> 1.50

--- src_new/adapters/prebid.js
+++ src_new/adapters/prebid.js
@@ -875,7 +875,7 @@ function getPbjsAdServerTargetingConfig(){
             key: "pwtecp", //hb_pb
             val: function(bidResponse) {
                 // return bidResponse.pbMg;
-                return bidResponse.cpm;
+                return window.parseFloat((bidResponse.cpm||0).toFixed(CONSTANTS.COMMON.BID_PRECISION));
             }
         }, {
             key: 'pwtsz', //hb_size
