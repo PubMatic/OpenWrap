526e3edcc2c3105f7f31cd42cab696e37d8366ec
Abhinav Sinha <abhinavsinha001@gmail.com>
2021-04-22 14:48:38 +0530

    Indentation fix

--- sample_pages/esp/eids-esp.1.0.js
+++ sample_pages/esp/eids-esp.1.0.js
@@ -1,39 +1,39 @@
 function fetchAsyncSignals(mode, customFunction, customKey) {
-    console.log("Going to fetch signals");
-    var eids = "";
-    switch (mode) {
-        case 1:
-            if (pbjs && pbjs.getUserIdsAsEids && typeof pbjs.getUserIdsAsEids === "function") {
-                eids = pbjs.getUserIdsAsEids(); // Get Identities from Prebid API in oRTB eids structure
-            }
-            break;
-        case 2:
-            if (owpbjs && owpbjs.getUserIdsAsEids && typeof owpbjs.getUserIdsAsEids === "function") {
-                eids = owpbjs.getUserIdsAsEids(); //Get Identities from Identity Hub  API in oRTB eids structure
-            }
-            break;
-        case 3:
-            if (typeof customFunction === "function") {
-                eids = customFunction(); //Get Identities from Custom function provided
-            }
-            break;
+	console.log("Going to fetch signals");
+	var eids = "";
+	switch (mode) {
+		case 1:
+			if (pbjs && pbjs.getUserIdsAsEids && typeof pbjs.getUserIdsAsEids === "function") {
+				eids = pbjs.getUserIdsAsEids(); // Get Identities from Prebid API in oRTB eids structure
+			}
+			break;
+		case 2:
+			if (owpbjs && owpbjs.getUserIdsAsEids && typeof owpbjs.getUserIdsAsEids === "function") {
+				eids = owpbjs.getUserIdsAsEids(); //Get Identities from Identity Hub  API in oRTB eids structure
+			}
+			break;
+		case 3:
+			if (typeof customFunction === "function") {
+				eids = customFunction(); //Get Identities from Custom function provided
+			}
+			break;
 
-        default:
-            eids = "Hello eids"; // Demo data
+		default:
+			eids = "Hello eids"; // Demo data
 
-    }
-    var dataKey = "eids" // Default key is eids
-    if (customKey && customKey.length > 0) {
-        dataKey = customKey; // Using custom key for data if passed  
-    }
-    var rawSignal = {}
-    rawSignal[dataKey] = eids;
-    signals = encryptSignals(JSON.stringify(rawSignal))
-    promise = Promise.resolve(signals);
-    console.log("fetching Signals: " + signals);
-    return promise;
+	}
+	var dataKey = "eids" // Default key is eids
+	if (customKey && customKey.length > 0) {
+		dataKey = customKey; // Using custom key for data if passed  
+	}
+	var rawSignal = {}
+	rawSignal[dataKey] = eids;
+	var signals = encryptSignals(JSON.stringify(rawSignal))
+	promise = Promise.resolve(signals);
+	console.log("fetching Signals: " + signals);
+	return promise;
 };
 
 function encryptSignals(signals) {
-    return btoa(signals); // Test encryption. To be replaced with better algo
+	return btoa(signals); // Test encryption. To be replaced with better algo
 };
