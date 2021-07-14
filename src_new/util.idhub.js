5affc2b8ac5f403bc2e0670dd50e5e14b8f165cf
Manasi Moghe <manasi.moghe@pubmatic.com>
2021-07-14 14:52:17 +0530

    exclude pubprovidedId from partner initialisation process for IH profile

--- src_new/util.idhub.js
+++ src_new/util.idhub.js
@@ -234,7 +234,9 @@ exports.addHookOnFunction = function (theObject, useProto, functionName, newFunc
 exports.getUserIdConfiguration = function () {
 	var userIdConfs = [];
 	refThis.forEachOnObject(CONFIG.getIdentityPartners(), function (parterId, partnerValues) {
-		userIdConfs.push(refThis.getUserIdParams(partnerValues));
+		if (!CONSTANTS.EXCLUDE_PARTNER_LIST.includes(parterId)) {
+			userIdConfs.push(refThis.getUserIdParams(partnerValues));
+		}
 	});
 	refThis.log(CONSTANTS.MESSAGES.IDENTITY.M4 + JSON.stringify(userIdConfs));
 	return userIdConfs;
