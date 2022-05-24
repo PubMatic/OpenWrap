bacc9b238c5f018e758e536f87a0f5c2acb9f569
Manasi Moghe <manasi.moghe@pubmatic.com>
2022-05-24 13:50:21 +0530

    changes log statements

--- src_new/ih_header.js
+++ src_new/ih_header.js
@@ -1,4 +1,3 @@
-console.log("########## DEBUG - ", window.owpbjs, " and pwt = ", window.PWT);
 if (window.owpbjs && window.PWT) { 
-    console.log('########## DEBUG - for identity only profile, detected duplicate owpbjs and pwt namespaces. exitinggggg'); 
+    console.log('DEBUG - for identity only profile, detected duplicate owpbjs and pwt namespaces. Exiting Identity Hub'); 
 } else {
