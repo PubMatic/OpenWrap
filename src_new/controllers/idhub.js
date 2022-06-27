a6f6d3aa62f6bfd399b050988b72746c54971abf
Nitin Nimbalkar <nitin.nimbalkar@pubmatic.com>
2022-06-27 09:19:55 +0530

    IDNT-112: JS changes-AuctionDelay as 1

--- src_new/controllers/idhub.js
+++ src_new/controllers/idhub.js
@@ -14,7 +14,8 @@ refThis.setConfig = function(){
 			var prebidConfig = {
 				debug: util.isDebugLogEnabled(),
 				userSync: {
-					syncDelay: 2000
+					syncDelay: 2000,
+					auctionDelay: 1,
 				}
 			};
 
