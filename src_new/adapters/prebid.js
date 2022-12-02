b2ce7c21d3d81916f41917352ac6ba44e3999921
Kapil Tuptewar <kapiltuptewar@L1290.local>
2022-12-02 12:22:33 +0530

    Changed platform to platform_id

--- src_new/adapters/prebid.js
+++ src_new/adapters/prebid.js
@@ -1072,7 +1072,7 @@ function getFloorsConfiguration(prebidConfig){
 				url: CONFIG.getFloorJsonUrl()
 			},
 			additionalSchemaFields : {
-				platform : util.getPltForFloor
+				platform_id : util.getPltForFloor
 			}
 		}
 	}
