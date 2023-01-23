f96d79deb7ce89356a65feef78d6bffc53f0117f
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2023-01-23 17:14:01 +0530

    Changes for gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 'use strict';
-console.time("Loading plugins");
+console.time("Loading plugins OW");
 var argv = require('yargs').argv;
 var gulp = require('gulp');
 var replace = require('gulp-replace-task');
@@ -79,9 +79,9 @@ gulp.task('webpack', gulp.series('clean', function() {
     var fsCache = require('gulp-fs-cache');
     var removeCode = require('gulp-remove-code');
     var jsFsCache = fsCache('.tmp/jscache');
-    webpackConfig.devtool = null;
+    webpackConfig.devtool = false;
 
-    return gulp.src(isIdentityOnly ? 'src_new/idhub.js' : 'src_new/owt.js')
+    return gulp.src(isIdentityOnly ? 'src_new/idhub.js' : 'src_new/owt.js', {"allowEmpty": true})
     // return gulp.src('src_new/owt.js')
         .pipe(webpack(webpackConfig))
         .pipe(jsFsCache)
@@ -101,7 +101,7 @@ gulp.task('webpack-creative', gulp.series('clean', function() {
     var webpack = require('webpack-stream');
     var webpackConfig = require('./webpack.config.js');
     var optimizejs = require('gulp-optimize-js');
-    webpackConfig.devtool = null;
+    webpackConfig.devtool = false;
 
     return gulp.src('src_new/creative/owCreativeRenderer.js')
         .pipe(webpack(webpackConfig))
@@ -122,7 +122,7 @@ gulp.task('devpack', gulp.series('clean', function () {
 
     webpackConfig.devtool = 'source-map';
 
-    return gulp.src(isIdentityOnly ? 'src_new/idhub.js' : 'src_new/owt.js')
+    return gulp.src(isIdentityOnly ? 'src_new/idhub.js' : 'src_new/owt.js', {"allowEmpty": true})
     // return gulp.src('src_new/owt.js')
         .pipe(webpack(webpackConfig))
         .pipe(removeCode(getRemoveCodeConfig()))
@@ -151,10 +151,10 @@ gulp.task('unexpose', function() {
 
 // Test all code without private functions
 gulp.task('test', gulp.series('unexpose', async function (done) {
-    var karma = require('gulp-karma');
+    // var karma = require('gulp-karma');
     var karmaServer = require('karma').Server;
 
-    var defaultBrowsers = CI_MODE ? ['PhantomJS'] : ['Chrome'];
+    var defaultBrowsers = CI_MODE ? ['ChromeHeadless'] : ['Chrome'];
     new karmaServer({
         browsers: defaultBrowsers,
         basePath: './temp',
@@ -179,7 +179,7 @@ gulp.task('test', gulp.series('unexpose', async function (done) {
 gulp.task('testall', async function (done) {
     // var karma = require('gulp-karma');
     var karmaServer = require('karma').Server;
-    var defaultBrowsers = CI_MODE ? ['PhantomJS'] : ['Chrome'];
+    var defaultBrowsers = CI_MODE ? ['ChromeHeadless'] : ['Chrome'];
     new karmaServer({
         browsers: defaultBrowsers,
         configFile: __dirname + '/karma.conf.js',
