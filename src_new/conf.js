af4a99b89cc0640902ac58fb9742301407c78e77
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2023-04-05 16:47:44 +0530

    Set GPP related config to prebid via OW

--- src_new/conf.js
+++ src_new/conf.js
@@ -24,7 +24,10 @@ exports.pwt = {
 	owv:"v21.4.0",
 	abTestEnabled:"0",
 	pubAnalyticsAdapter: "0",
-	reduceCodeSize:1
+	reduceCodeSize:1,
+	gpp: "0",
+	gppCmpApi: "iab",
+	gppTimeout: "10000"
 };
 
 // singleImpression is used to enable feature of sending single impression for multiple size ad slot earlier there were multiple impression for multiple sizes
