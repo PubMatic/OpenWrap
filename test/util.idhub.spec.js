b1c666f69db08d7c7f2fa9766135b8f9c5118d73
Nitin Nimbalkar <nitin.nimbalkar@pubmatic.com>
2023-05-10 14:57:40 +0530

    IDNT override ids fix test cases added

--- test/util.idhub.spec.js
+++ test/util.idhub.spec.js
@@ -1178,7 +1178,7 @@ describe('IDHUBUTIL', function() {
                 'ecpm':'10.00'
             }
             sinon.stub(IDHUBUTIL,'getUserIds').returns({id:1})
-            sinon.stub(IDHUBUTIL,'getUserIdsAsEids').returns([{"source":"myId",id:1}])
+            sinon.stub(IDHUBUTIL,'getUserIdsAsEids').returns([{"source":"myId",uids:[{ "id": "idhub-id"}]}])
             done();
         });
 
@@ -1195,31 +1195,31 @@ describe('IDHUBUTIL', function() {
         });
 
         it('should add UserId in bid if userIds is not present', function(done){
-            var expectedResult = {"ecpm":"10.00","userId":{"id":1},"userIdAsEids":[{"source":"myId","id":1}]}
+            var expectedResult = {"ecpm":"10.00","userId":{"id":1},"userIdAsEids":[{"source":"myId",uids:[{ "id": "idhub-id"}]}]}
             IDHUBUTIL.updateUserIds(bid)
             bid.should.be.deep.equal(expectedResult);
             done();
         })
 
-        // TODO: UnComment Below Test Cases once PhantomJs is replaced by ChromeHeadless in build.sh production and test mode
-        xit('should update UserID in bid if userIds is present',function(done){
-            var expectedResult = {"ecpm":"10.00","userId":{"existingId":2,"id":1},"userIdAsEids":[{"source":"myId","id":1},{"source":"existingMyId","existingId":2}]} 
+        it('should update(or merge) UserID in bid if userIds is present',function(done){
+            var expectedResult = {"ecpm":"10.00","userId":{"existingId":2,"id":1},"userIdAsEids":[{"source":"existingMyId","uids": [{"id": "prebid-id"}]},{"source":"myId",uids:[{ "id": "idhub-id"}]}]} 
             bid['userId'] = {"existingId":2}
-            bid['userIdAsEids'] = [{"source":"existingMyId","existingId":2}]
+            bid['userIdAsEids'] = [{"source":"existingMyId","uids": [{"id": "prebid-id"}]}]
             IDHUBUTIL.updateUserIds(bid)
             bid.should.be.deep.equal(expectedResult);
             done();
         })
-        // TODO: UnComment Below Test Cases once PhantomJs is replaced by ChromeHeadless in build.sh production and test mode
-        xit('should update with IH values if same id is present', function(done){
-            var expectedResult = {"ecpm":"10.00","userId":{"id":1},"userIdAsEids":[{"source":"myId","id":1}]}
+        
+        it('should update or pass prebid ID instead of IH ID in bid if same id is present', function(done){
+            var expectedResult = {"ecpm":"10.00","userId":{"id":1},"userIdAsEids":[{"source":"myId","uids": [{"id": "prebid-id"}]}]}
             bid['userId'] = {"id":2}
-            bid['userIdAsEids'] = [{"source":"myId","id":2}]
+            bid['userIdAsEids'] = [{"source":"myId","uids": [{"id": "prebid-id"}]}]
             IDHUBUTIL.updateUserIds(bid);
             bid.should.be.deep.equal(expectedResult);
             done();
         })
-    })
+    });
+    
     describe('#applyDataTypeChangesIfApplicable', function() {
         var params;
         beforeEach(function(done) {
