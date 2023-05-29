137a0e1028891617bd935638446fae867b99aa96
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2023-05-29 16:00:14 +0530

    REmove GPP changes

--- test/adapters/prebid.spec.js
+++ test/adapters/prebid.spec.js
@@ -1279,9 +1279,6 @@ describe('ADAPTER: Prebid', function() {
             sinon.spy(CONFIG, 'getCCPATimeout');
             sinon.stub(CONFIG, 'getCCPA').returns(true);
 
-            sinon.spy(CONFIG, 'getGppTimeout');
-            sinon.spy(CONFIG, 'getGppCmpApi');
-            sinon.stub(CONFIG, 'getGppConsent').returns(true);
 
             sinon.stub(BM, 'resetBid', function(){});
             sinon.stub(BM, 'setSizes', function(){});
@@ -1352,10 +1349,6 @@ describe('ADAPTER: Prebid', function() {
 
             CONFIG.getCCPA.restore();
             CONFIG.getCCPATimeout.restore();
-
-            CONFIG.getGppConsent.restore();
-            CONFIG.getGppCmpApi.restore();
-            CONFIG.getGppTimeout.restore();
 
             BM.resetBid.restore();
             BM.setSizes.restore();
@@ -1428,9 +1421,6 @@ describe('ADAPTER: Prebid', function() {
             CONFIG.getCCPA().should.be.true;
             CONFIG.getCCPACmpApi().should.be.called;
             CONFIG.getCCPATimeout().should.be.called;
-            CONFIG.getGppConsent().should.be.true;
-            CONFIG.getGppCmpApi().should.be.called;
-            CONFIG.getGppTimeout().should.be.called;
             done();
         });
 
