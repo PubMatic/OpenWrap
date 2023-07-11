bcdadc74fc56e207e214a65191ef33ddde0a5ba6
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2023-07-11 14:57:59 +0530

    Reverted to original values

--- src_new/conf.js
+++ src_new/conf.js
@@ -25,8 +25,8 @@ exports.pwt = {
 	abTestEnabled:"0",
 	pubAnalyticsAdapter: "0",
 	reduceCodeSize:1,
-	pbGlobalVarNamespace: "custPbNamespace",
-	owGlobalVarNamespace: "NIT"
+	pbGlobalVarNamespace: "owpbjs",
+	owGlobalVarNamespace: "PWT"
 };
 
 // singleImpression is used to enable feature of sending single impression for multiple size ad slot earlier there were multiple impression for multiple sizes
