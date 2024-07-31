f0424bc35260a2f17ff4703bdc562cff3146cfb4
pm-priyanka-deshmane <priyanka.deshmane@pubmatic.com>
2024-07-31 18:38:34 +0530

    Changes for demo

--- src_new/conf.js
+++ src_new/conf.js
@@ -1,246 +1,206 @@
 exports.pwt = {
-	t: "3000",
-	pid: "46",
-	gcv: "11",
-	pdvid: "4",
-	pubid: "9999",
-	dataURL: "t.test-domain.com/logger?",
-	winURL: "t.test-domain.com/tracker?",
-	adserver: "CUSTOM",
-	gdpr: "1",
-	cmpApi: "iab",
-	gdprTimeout: "10000",
-	awc: "1",
-	disableAjaxTimeout: true,
-	adServerCurrency: "INR",
+	pid: "66622",
+	gcv: "209",
+	pdvid: "1",
+	pubid: "5890",
+	dataURL: "t.pubmatic.com/wl?",
+	winURL: "t.pubmatic.com/wt?",
+	owv: "v26.10.0",
+	pbv: "v7.39.0",
+	usePBSAdapter: "0",
+	reduceCodeSize: "1",
+	metaDataPattern: 0,
+	sendAllBids: "0",
+	adserver: "DFP",
+	gdpr: "0",
+	cmp: 0,
+	gdprTimeout: 0,
+	awc: 0,
+	platform: "display",
+	refreshInterval: 0,
+	priceGranularity: 0,
+	adServerCurrency: 0,
 	singleImpression: "1",
-	identityEnabled: "1",
-	identityConsumers: "EB,TAM,Prebid",
-	identityOnly: "0",
-	ccpa: "1",
-	ccpaCmpApi: "iab",
-	ccpaTimeout: "10000",
-	gpp: "1",
-	gppTimeout: "1000",
-	pbv:"v4.33.0",
-	owv:"v21.4.0",
-	abTestEnabled:"0",
-	pubAnalyticsAdapter: "0",
-	reduceCodeSize:1,
+	identityEnabled: 1,
+	identityConsumers: 0,
+	ccpa: "0",
+	ccpaCmpApi: 0,
+	ccpaTimeout: 0,
+	sChain: "0",
+	sChainObj: 0,
+	auTimeout: "1000",
+	t: "1000",
+	ssTimeout: 0,
+	prebidObjName: 0,
+	pubAnalyticsAdapter: "1",
+	usePBJSKeys: "0",
+	abTestEnabled: "0",
+	testGroupSize: 0,
+	testType: 0,
+	granularityMultiplier: 0,
+	floorPriceModuleEnabled: "0",
+	floorSource: 0,
+	floorAuctionDelay: 0,
+	jsonUrl: 0,
+	ssoEnabled: 0,
+	autoRefreshAdslots: "0",
+	videoAdDuration: 0,
+	videoAdDurationMatching: 0,
+	adPodConfiguration: 0,
+	customPriceGranularityConfig: 0,
+	marketplaceBidders: 0,
+	owRedirectURL: 0,
+	topicsFPDModule: 0,
+	enableVastUnwrapper: 0,
+	floorType: 0,
+	pubId: 0,
+	zone: 0,
 	pbGlobalVarNamespace: "owpbjs",
 	owGlobalVarNamespace: "PWT",
-	localStorageAccess: "1", // Added new field for allow local storage feature 
+	gpp: "0",
+	gppCmpApi: 0,
+	gppTimeout: 0,
+	globalNamespaceType: "Default",
+	gdprActionTimeout: 0,
+	localStorageAccess: "1",
+	 // Added new field for allow local storage feature 
 	filters: {
-        'idpartners': {
-            'id5Id': {
-                geo: {
-                    allowList: ['CCPA'] // run only in US, block everywhere else
-                }
-            },
-            'merkleId': {
-               geo: {
-                    allowList: ['GDPR'] // run only in EU, block everywhere else
-               }
-            },
-            'zeotapIdPlus': {
-                geo: {
-                    allowList: ['CA', 'TX', 'NY', 'FL', 'IL'] // run only in these country codes, block everywhere else
-                }
-            },
-            'hadronId': {
-                domain: {
-                    blockList: ['127.0.0.1:8081'] // block on this domain
-                } 
-            }
-        },
-        'adapters': {
-            'pubmatic': {
-               geo: {
-                    blockList: ['GDPR']
-               }
-            },
-            'appnexus': {
-                geo: {
-                    allowList: ['CCPA']
-                }
-            },
-            'rubicon': {
-                geo: {
-                    allowList: ['GDPR']
-                },
-                domain: {
-                    blockList: ['127.0.0.1:8081'] // block on this domain
-                }
-            }
-        }
-  },
-	dynamicTimeoutEnabled: "1",
-	autoMoveBidder: "1",
-	doNotMoveBidders: ["appnexus", "pubmatic"],
-};
-
-// singleImpression is used to enable feature of sending single impression for multiple size ad slot earlier there were multiple impression for multiple sizes
-
-//below is the config for test purpose only
-exports.testConfigDetails = {
-	"testGroupSize": 99
-};
-//below is the config for test purpose only
-exports.test_pwt = {
-	"t": 5000
-};
-exports.adapters = 
-{
-	pubmatic: {
-		rev_share: "0.0",
-		throttle: "100",
-		publisherId: "156209",
-		kgp: "_W_x_H_@_W_x_H_:_AUI_"
+	   'idpartners': {
+		 'identityLink': {
+		   geo: {
+			 allowList: ['CCPA'] // run only in US, block everywhere else
+		   }
+		 },
+		 'id5Id': {
+		   geo: {
+			 allowList: ['GDPR'] // run only in EU, block everywhere else
+		   }
+		 },
+		 'liveIntentId': {
+		   domain: {
+			 blockList: ['localhost'] // block on this domain
+		   }
+		 }
+	   },
+	   'adapters': {
+		 'rubicon': {
+		   geo: {
+			 allowList: ['GDPR']
+		   },
+		   domain: {
+			 blockList: ['localhost'] // block on this domain
+		   }
+		 }
+	   }
+	 },
+	 dynamicTimeoutEnabled: "1",
+	 autoMoveBidder: "1",
+	 doNotMoveBidders: ["appnexus", "pubmatic"]
+   };
+   
+   // singleImpression is used to enable feature of sending single impression for multiple size ad slot earlier there were multiple impression for multiple sizes
+   
+   //below is the config for test purpose only
+   exports.testConfigDetails = {
+	 "testGroupSize": 99
+   };
+   //below is the config for test purpose only
+   exports.test_pwt = {
+	 "t": 5000
+   };
+   exports.adapters = {
+	 pubmatic: {
+	   rev_share: "0.0",
+	   throttle: "100",
+	   publisherId: "156209",
+	   kgp: "_W_x_H_@_W_x_H_:_AUI_"
+	 },
+	 appnexus: {
+	   rev_share: "0.0",
+	   throttle: "100",
+	   kgp: "_DIV_",
+	   klm: {
+		 "Div_1": {
+		   placementId: "8801674",
+		   "video.mimes": "",
+		   "video.minduration": ""
+		 },
+		 "Div-2": {
+		   placementId: "8801685"
+		 }
+	   }
+	 },
+	 rubicon: {
+	   accountId: "10998",
+	   rev_share: "0.0",
+	   timeout: "1000",
+	   throttle: "100",
+	   pt: 0,
+	   serverSideEnabled: "0",
+	   amp: 0,
+	   video: 0,
+	   "in-app": 0,
+	   kgp_rx: "_AU_@_DIV_@_W_x_H_",
+	   klm_rx: [{
+		 rx: {
+		   DIV: ".*",
+		   AU: "^/43743431/DMDemo",
+		   SIZE: "728x90"
+		 },
+		 rx_config: {
+		   zoneId: "869224",
+		   siteId: "178620",
+		   floor: "0"
+		 }
+	   }]
+	 }
+   };
+   exports.identityPartners = {
+	 id5Id: {
+	   name: "id5Id",
+	   "storage.type": "html5",
+	   "storage.expires": "90",
+	   "storage.name": "id5id",
+	   "storage.refreshInSeconds": "28800",
+	   "params.partner": "556",
+	   "params.abTesting.enabled": 0,
+	   "params.abTesting.controlGroupPct": 0,
+	   rev_share: "0.0",
+	   timeout: 0,
+	   throttle: "100",
+	   display: 0
+	 },
+	 liveIntentId: {
+		name: "liveIntentId",
+		"params.publisherId": 5890,
+		"params.requestedAttributesOverrides": '{"uid2":true,"pubmatic":true}',
+		"storage.type": "cookie",
+		"storage.name": "pbjs_li_nonid"
 	},
-	appnexus: {
-		rev_share: "0.0",
-		throttle: "100",
-		kgp: "_DIV_",
-		klm: {
-			"Div_1": {
-				placementId: "8801674",
-				"video.mimes": "",
-				"video.minduration": ""
-			},
-			"Div-2": {
-				placementId: "8801685"
-			}
-		}
+	identityLink: {
+		name: "identityLink",
+		"storage.type": "cookie",
+		"storage.expires": "30",
+		"storage.name": "idl_env",
+		"params.pid": "198",
+		"params.loadATS": 0,
+		"params.storageType": 0,
+		"params.detectionType": 0,
+		"params.urlParameter": 0,
+		"params.cssSelectors": 0,
+		"params.logging": 0,
+		"params.detectDynamicNodes": 0,
+		"params.enableCustomId": 0,
+		"params.accountID": 0,
+		"params.customerIDRegex": 0,
+		"params.detectionMechanism": 0,
+		"params.detectionEventType": 0,
+		"params.triggerElements": 0,
+		"params.pixelID": 0,
+		"rev_share": "0.0",
+		"timeout": 0,
+		"throttle": "100",
+		"display": 0
 	},
-	rubicon: {
-		accountId: "10998",
-		rev_share: "0.0",
-		timeout: "1000",
-		throttle: "100",
-		pt: 0,
-		serverSideEnabled: "0",
-		amp: 0,
-		video: 0,
-		"in-app": 0,
-		kgp_rx: "_AU_@_DIV_@_W_x_H_",
-		klm_rx: [{
-			rx: {
-				DIV: ".*",
-				AU: "^/43743431/DMDemo",
-				SIZE: "728x90"
-			},
-			rx_config: {
-				zoneId: "869224",
-				siteId: "178620",
-				floor: "0"
-			}
-		}]
-	}
-};
-
-exports.identityPartners = {
-	id5Id: {
-		name: "id5Id",
-		"storage.type": "html5",
-		"storage.expires": "90",
-		"storage.name": "id5id",
-		"storage.refreshInSeconds": "28800",
-		"params.partner": "556",
-		"params.abTesting.enabled": 0,
-		"params.abTesting.controlGroupPct": 0,
-  	    rev_share: "0.0",
-		timeout: 0,
-		throttle: "100",
-		display: 0
-    },
-    zeotapIdPlus: {
-		name:"zeotapIdPlus",
-		"params.loadIDP":0,
-		partnerId:0,
-		rev_share:"0.0",
-		timeout:0,
-		throttle:"100",
-		display:0
-	},
-	hadronId: {
-		name: "hadronId",
-		"params.url": "https://prebid.org/hadron_id/empty",
-		"params.urlArg": 0,
-		"params.partnerId": "1234",
-		display: 0,
-		rev_share: "0.0",
-		throttle: "100"
-	},
-	merkleId: {
-		name:"merkleId",
-		"params.sv_pubid":"5890",
-		"params.ssp_ids":"76,89",
-		"storage.name":"_svsid",
-		"storage.type":"cookie"
-	}
-};
-
-
-/// AD UNIT AU1
-// Read Config File and Get Video Config
-// 1. Video Config is available 
-// 2. Check if Defaut Video is Enabled or not
-// 3. Generate Config of slot based on KGP of Default Video it would be _AU_ // AU1
-// 4. Loop on slotConfig for that generated slot config in pt.3
-// 5. DIV1 -> Apply based on condtions (enabled,)
-// 6. DIV5 -> It will increase Latency 
-
-exports.slotConfig = {
-	"configPattern": "_DIV_",
-	"config": {
-		"Div1": {
-			"banner": {
-				"enabled": true
-			},
-			"native": {
-				"enabled": true,
-				"config": {
-					"image": {
-						"required": true,
-						"sizes": [150, 50]
-					},
-					"title": {
-						"required": true,
-						"len": 80
-					},
-					"sponsoredBy": {
-						"required": true
-					},
-					"body": {
-						"required": true
-					}
-				}
-			},
-			"video": {
-				"enabled": true,
-				"config": {
-					"context": "instream",
-					"connectiontype": [1, 2, 6],
-					"minduration": 10,
-					"maxduration": 50,
-					"battr": [
-						6,
-						7
-					],
-					"skip": 1,
-					"skipmin": 10,
-					"skipafter": 15
-				},
-				"partnerConfig": {
-					"pubmatic": {
-						"outstreamAU": "pubmatic-test"
-					}
-				}
-			},
-
-		},
-		"AU2": {
-			"banner": {}
-		}
-	}
-};
+   };
+  
--- src_new/preloader.js
+++ src_new/preloader.js
@@ -16,25 +16,25 @@ var adapters = config.getAdapters();
 function getGeoInfoNew () {
 	var geos = ['GDPR','CCPA','GPP','ROW', 'CA', 'TX', 'NY', 'FL', 'IL'];
 	var randomValue = geos[Math.floor(Math.random() * geos.length)];
-     console.log(`Random geo: ${randomValue}`);   
+     console.log("Random geo: " + randomValue);
 	return {'geo': randomValue};
 }
 
 var response = getGeoInfoNew();
 var currentDomain = window.location.hostname;
 
-function shouldDelete(partner, geo, domain) {
+function shouldDelete(partner, geo, domain, key) {
     if (geo) {
         if ((geo.allowList && !geo.allowList.includes(response.geo))
             || (geo.blockList && geo.blockList.includes(response.geo))) {
-            console.log('Deleting due to allowList mismatch: ', partner);
+            console.log('Deleting due to allowList mismatch: ', key);
             return true;
         }
     }
     if (domain) {
         if ((domain.allowList && !domain.allowList.includes(currentDomain))
             || (domain.blockList && domain.blockList.includes(currentDomain))) {
-            console.log('Deleting due to domain blockList match: ', partner);
+            console.log('Deleting due to domain blockList match: ', key);
             return true;
         }
         return false;
@@ -43,19 +43,19 @@ function shouldDelete(partner, geo, domain) {
 
 for (var key in filters.idpartners) {
     var idpartner = filters.idpartners[key];
-    if (shouldDelete(idpartner, idpartner.geo, idpartner.domain)) {
+    if (shouldDelete(idpartner, idpartner.geo, idpartner.domain, key)) {
         delete identityPartners[key];
     } else {
-        console.log(`Allowed: ${key}`);
+        console.log("Allowed: " + key);
     }
 }
 
 for (var key in filters.adapters) {
     var adapter = filters.adapters[key];
-    if (shouldDelete(adapter, adapter.geo, adapter.domain)) {
+    if (shouldDelete(adapter, adapter.geo, adapter.domain, key)) {
         delete adapters[key];
     } else {
-        console.log(`Allowed: ${key}`);
+        console.log("Allowed: " + key);
     }
 }
 
@@ -86,7 +86,8 @@ if (config.isDynamicTimeoutEnabled()) {
     if (connectionTimeoutMap.hasOwnProperty(connectionType)) {
         var timeoutToAdd = connectionTimeoutMap[connectionType];
         var timeout = config.getTimeout() + timeoutToAdd;
-        console.log(`Timeout increased by ${timeoutToAdd} ms due to connection type: ${connectionType} and new timeout is ${timeout}`);
+        console.log("Timeout increased by " + timeoutToAdd + " ms due to connection type: " + connectionType +
+        " and new timeout is " + timeout);
         config.setTimeout(timeout);
     }
 }
@@ -104,15 +105,15 @@ if (config.getAutoMoveBidder()) {
             try {
                 // Parse the data as JSON (if it's a JSON string)
                 const parsedData = JSON.parse(data);
-                console.log(`Data for key "${key}":`, parsedData);
+                console.log("Data for key " + key + ":" + parsedData);
                 return parsedData;
             } catch (e) {
                 // If parsing fails, return the raw string data
-                console.log(`Data for key "${key}":`, data);
+                console.log("Data for key " + key + ":," + data);
                 return data;
             }
         } else {
-            console.log(`No data found for key "${key}".`);
+            console.log("No data found for key " + key);
             return null;
         }
     }
@@ -121,15 +122,15 @@ if (config.getAutoMoveBidder()) {
     const bidderSettings = readFromLocalStorage('bidderSettings');
 
     if (bidderSettings) {
-        let thresholds = {
+        var thresholds = {
             rc: 10,
             tc: 4,
             l: 10000
         };
-        let doNotMoveBidders = config.getDoNotMoveBidders();
+        var doNotMoveBidders = config.getDoNotMoveBidders();
 
         function filterObjectByValue(bidderSettings, thresholds) {
-            for (let key in bidderSettings) {                
+            for (var key in bidderSettings) {                
                 if(doNotMoveBidders.includes(key)) {
                     continue;
                 }
@@ -148,11 +149,11 @@ if (config.getAutoMoveBidder()) {
 // ServerSideEnabled UPDATE CODE ENDS
 
 // CMP FIND CODE STARTS
-let win = window;
+var win = window;
 function findCMP(apiName) {
-    let f = win;
-    let cmpFrame;
-    let isCmpPresent = false;
+    var f = win;
+    var cmpFrame;
+    var isCmpPresent = false;
     while (f != null) {
         try {
             if (typeof f[apiName] === 'function') {
@@ -165,7 +166,7 @@ function findCMP(apiName) {
 
         // need separate try/catch blocks due to the exception errors thrown when trying to check for a frame that doesn't exist in 3rd party env
         try {
-            if (f.frames[`${apiName}Locator`]) {
+            if (f.frames[apiName+ "Locator"]) {
                 cmpFrame = f;
                 isCmpPresent = true;
                 break;
@@ -179,7 +180,7 @@ function findCMP(apiName) {
     return isCmpPresent;
 };
 
-let cmpAPIs = {
+var cmpAPIs = {
     '__tcfapi': false,
     '__uspapi': false,
     '__gpp': false
