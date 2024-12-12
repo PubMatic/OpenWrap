2932e970ba417ab5fe760c0f97969c64bd1fff5f
Nitin Shirsat <nitin.shirsat@pubmatic.com>
2024-12-12 09:13:48 +0530

    Gpp Phase 02: Added time metrics module

---
+++ src_new/modules/timeMetrics.js
@@ -0,0 +1,59 @@
+var commonUtil = require('../common.util.js');
+
+// Get a metrics object within PWT
+function getMetricsObject() {
+  commonUtil.getGlobalOwObject().metrics = commonUtil.getGlobalOwObject().metrics || {};
+  return commonUtil.getGlobalOwObject().metrics;
+}
+
+// Function to set entry and exit times for a specific module and/or function
+function setMetrics(options) {
+  if (options.moduleName) {
+    if (!getMetricsObject()[options.moduleName]) {
+      getMetricsObject()[options.moduleName] = {};
+    }
+    if (options.functionName) {
+      getMetricsObject()[options.moduleName][options.functionName] = {
+        entryTime: options.entryTime || null,
+        exitTime: options.exitTime || null
+      };
+    } else {
+      getMetricsObject()[options.moduleName] = {
+        entryTime: options.entryTime || null,
+        exitTime: options.exitTime || null
+      };
+    }
+  } else if (options.functionName) {
+    getMetricsObject()[options.functionName] = {
+      entryTime: options.entryTime || null,
+      exitTime: options.exitTime || null
+    };
+  }
+}
+
+// Function to get metrics for a specific module and/or function
+function getMetrics(moduleName, functionName) {
+  if (moduleName && functionName) {
+    return (getMetricsObject()[moduleName] && getMetricsObject()[moduleName][functionName]) || null;
+  } else if (moduleName) {
+    return getMetricsObject()[moduleName] || null;
+  } else if (functionName) {
+    return getMetricsObject()[functionName] || null;
+  }
+  return null;
+}
+
+// Function to record entry time
+exports.recordEntryTime = function(moduleName, functionName) {
+  var currentTime = new Date().getTime();
+  setMetrics({ moduleName: moduleName, functionName: functionName, entryTime: currentTime });
+}
+
+// Function to record exit time
+exports.recordExitTime = function(moduleName, functionName) {
+  var currentTime = new Date().getTime();
+  var metrics = getMetrics(moduleName, functionName);
+  if (metrics) {
+    metrics.exitTime = currentTime;
+  }
+}
