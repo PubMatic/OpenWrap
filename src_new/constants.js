c915288ca4710b392a14dfa3960ad2f64ae6862c
pm-priyanka-deshmane <107103300+pm-priyanka-deshmane@users.noreply.github.com>
2025-09-10 13:04:30 +0530

    Updated constants.js to change the yieldOptiEnabled key

--- src_new/constants.js
+++ src_new/constants.js
@@ -120,7 +120,7 @@ exports.CONFIG = {
 	"FLOOR_JSON_URL":"jsonUrl",
 	"FLOOR_ENFORCE_JS":"floorType",
 	"DEFAULT_FLOOR_ENFORCE_JS": true,
-	"YIELD_OPTIMIZER_ENABLED": "yieldOptimizerEnabled",
+	"YIELD_OPTIMIZER_ENABLED": "yieldOptiEnabled",
 	"USE_PREBID_KEYS": "usePBJSKeys",
 	"AB_TEST_ENABLED": "abTestEnabled",
 	"TIMEOUT_ADJUSTMENT": 50,
